f2f4cb227742497c058c095b4f1440225199e312
giulian <giulian.fazio@gmail.com>
2020-03-20 17:33:40 +0100

    set different color options for keyboard layout

--- widget/myboxlayout.py
+++ widget/myboxlayout.py
@@ -2,6 +2,10 @@ from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ListProperty
 
+black = [0, 0, 0, 1]
+orange = [1, 0.6, 0.1, 1]
+white = [1, 1, 1, 1]
+gray = [0.77, 0.77, 0.77, 1]
 
 Builder.load_string('''
 <MyBoxLayout>
@@ -16,18 +20,20 @@ Builder.load_string('''
         Line:
             width: 2
             rectangle: self.x, self.y, self.width, self.height
+<Label>
+    color: [0, 0, 0, 1]
 ''')
 
 class MyBoxLayout(BoxLayout):
-    black = [0, 0, 0, 1]
-    orange = [1, 0.6, 0.1, 1]
+    default_background_color = gray
+    target_background_color = orange
 
-    background_color = ListProperty(black)
+    background_color = ListProperty(default_background_color)
 
     def target_it(self):
-        self.background_color = self.orange
+        self.background_color = self.target_background_color
 
     def untarget_it(self):
-        self.background_color = self.black
+        self.background_color = self.default_background_color
 
     pass
